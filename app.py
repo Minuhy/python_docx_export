faace11a4d5939a4bc0981bf5f2297a86b729dd4
Minuhy <y17mmz@live.com>
2023-06-05 13:56:31 +0800

    GUI

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import logging
 import os
 import tkinter as tk
+from tkinter import ttk
 from datetime import datetime
 from tkinter import filedialog
-from tkinter.messagebox import askokcancel, askyesno, showinfo
+from tkinter.messagebox import askokcancel, askyesno, showinfo, showwarning
 
 v = 'v 0.1.0.1'
 
@@ -54,18 +55,45 @@ class Application(tk.Frame):
         super().__init__(master)
         self.master = master
         self.pack()
+        self.version = version
+
+        # 待处理文件列表
         self.file_list = []
+        # 文件添加索引
+        self.file_index = 0
 
-        label = tk.Label(self, text='批量导出docx文本、图片和附件程序\n' + version, font=('宋体', 18, 'bold italic'), bg='#7CCD7C',
-                         # 设置标签内容区大小
-                         width=34, height=2,
-                         # 设置填充区距离、边框宽度和其样式（凹陷式）
-                         padx=10, pady=15, borderwidth=10, relief='sunken')
-        label.pack()
+        # 主列表控件（显示待处理文件）
+        self.main_listbox = None
 
-        # 主窗口流程
-        choose_frame = tk.Frame(self)
-        choose_frame.pack()
+        # 导出目录
+        self.entry_export_dir_val = tk.StringVar()
+
+        # 导出类型
+        self.check_export_text = tk.IntVar()
+        self.check_export_table = tk.IntVar()
+        self.check_export_image = tk.IntVar()
+        self.check_export_attachment = tk.IntVar()
+
+        # 创建控件，左右
+        main_frame = tk.Frame(self)
+
+        self.create_left(main_frame)
+        self.create_right(main_frame)
+
+        main_frame.pack(expand=1, fill=tk.BOTH, padx=10, pady=10)
+
+    def create_left(self, frame):
+        """
+        左边布局，文件列表
+        :return: None
+        """
+        main_left_frame = tk.Frame(frame)
+
+        # 标签面板
+        list_frame_label = tk.LabelFrame(main_left_frame, text='待处理文件列表')
+
+        # 选择面板
+        choose_frame = tk.Frame(list_frame_label)
 
         tk.Button(choose_frame,
                   text="选择文件",
@@ -81,45 +109,217 @@ class Application(tk.Frame):
                   command=self.choose_dir) \
             .grid(row=0, column=1)
 
-        list_frame = tk.Frame(self)
-        list_frame.pack()
-        scroll_bar = tk.Scrollbar(list_frame)  # 垂直滚动条组件
-        scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)  # 设置垂直滚动条显示的位置
+        # 移除选项按钮
+        tk.Button(choose_frame,
+                  text='移除列表选中项',
+                  command=self.remove_list_item) \
+            .grid(row=0, column=4)
+
+        choose_frame.pack()
+
+        # 一个列表
+        list_frame = tk.Frame(list_frame_label)
+        scroll_h_bar = tk.Scrollbar(list_frame, orient=tk.HORIZONTAL)  # 水平滚动条组件
+        scroll_v_bar = tk.Scrollbar(list_frame, orient=tk.VERTICAL)  # 垂直滚动条组件
         self.main_listbox = tk.Listbox(list_frame,
                                        width=60, height=20,
-                                       yscrollcommand=scroll_bar.set)
+                                       selectmode=tk.MULTIPLE,
+                                       yscrollcommand=scroll_v_bar.set,
+                                       xscrollcommand=scroll_h_bar.set)
+        scroll_v_bar.pack(side=tk.RIGHT, fill=tk.Y)  # 设置垂直滚动条显示的位置
+        scroll_v_bar.config(command=self.main_listbox.yview)  # 设置Scrollbar组件的command选项为该组件的yview()方法
+        scroll_h_bar.pack(side=tk.BOTTOM, fill=tk.X)  # 设置水平滚动条显示的位置
+        scroll_h_bar.config(command=self.main_listbox.xview)  # 设置Scrollbar组件的command选项为该组件的xview()方法
         self.main_listbox.pack(side=tk.LEFT, fill=tk.BOTH)
-        scroll_bar.config(command=self.main_listbox.yview)  # 设置Scrollbar组件的command选项为该组件的yview()方法
+        list_frame.pack(side=tk.TOP)
+        list_frame_label.pack(expand=True)
 
-        # 退出按钮
-        tk.Button(self,
-                  text="退出",
-                  fg="red",
+        main_left_frame.grid_rowconfigure(0, weight=1)
+        main_left_frame.grid_columnconfigure(0, weight=1)
+        main_left_frame.grid_columnconfigure(1, weight=1)
+        main_left_frame.grid_rowconfigure(1, weight=1)
+        main_left_frame.grid(row=0, column=0, sticky='n')
+
+    def create_right(self, frame):
+        """
+        右边布局
+        :return: None
+        """
+        main_right_frame = tk.Frame(frame)
+
+        # 导出设置=====================================================================================================
+        export_setting_frame = tk.LabelFrame(main_right_frame, text='导出设置')
+        # 导出下拉框
+        tk.Label(export_setting_frame, text="保存位置：").grid(row=0, column=0)
+        # 创建下拉菜单
+        combobox = ttk.Combobox(export_setting_frame, width=32, state="readonly")
+        # 设置下拉菜单中的值
+        combobox['value'] = ('原文件目录中以原文件命名的子文件夹中', '原文件所在文件夹', '自定义文件夹')
+        # 使用 grid() 来控制控件的位置
+        combobox.grid(row=0, column=1)
+        # 通过 current() 设置下拉菜单选项的默认值
+        combobox.current(0)
+
+        # 选择导出目录按钮
+        tk.Button(export_setting_frame,
+                  text="选择导出文件夹",
                   width=15, height=1,
-                  command=self.exit_app) \
-            .pack(side=tk.BOTTOM)
+                  command=self.choose_export_dir) \
+            .grid(row=0, column=2)
 
-    def add_file_list(self, file_list: list):
+        # 导出目录输入框
+        tk.Entry(export_setting_frame,
+                 width=60,
+                 textvariable=self.entry_export_dir_val) \
+            .grid(row=1, column=0, columnspan=3)
+
+        # 导出下拉框
+        tk.Label(export_setting_frame, text="导出文件名设置：") \
+            .grid(row=2, column=0)
+
+        # 创建下拉菜单
+        combobox = ttk.Combobox(export_setting_frame, width=8)
+        # 设置下拉菜单中的值
+        combobox['value'] = ('|自增编号|', '|原文件名|', '|后缀名|', '|连接符|')
+        # 使用 grid() 来控制控件的位置
+        combobox.grid(row=3, column=0)
+        # 通过 current() 设置下拉菜单选项的默认值
+        combobox.current(0)
+
+        # 创建下拉菜单
+        combobox = ttk.Combobox(export_setting_frame, width=8)
+        # 设置下拉菜单中的值
+        combobox['value'] = ('|自增编号|', '|原文件名|', '|后缀名|', '|连接符|')
+        # 使用 grid() 来控制控件的位置
+        combobox.grid(row=3, column=1)
+        # 通过 current() 设置下拉菜单选项的默认值
+        combobox.current(0)
+
+        # 创建下拉菜单
+        combobox = ttk.Combobox(export_setting_frame, width=8)
+        # 设置下拉菜单中的值
+        combobox['value'] = ('|自增编号|', '|原文件名|', '|后缀名|', '|连接符|')
+        # 使用 grid() 来控制控件的位置
+        combobox.grid(row=3, column=2)
+        # 通过 current() 设置下拉菜单选项的默认值
+        combobox.current(0)
+
+        # 创建下拉菜单
+        combobox = ttk.Combobox(export_setting_frame, width=8)
+        # 设置下拉菜单中的值
+        combobox['value'] = ('|自增编号|', '|原文件名|', '|后缀名|', '|连接符|')
+        # 使用 grid() 来控制控件的位置
+        combobox.grid(row=3, column=3)
+        # 通过 current() 设置下拉菜单选项的默认值
+        combobox.current(0)
+
+        check_box_frame = tk.Frame(export_setting_frame)
+        # 复选框控件，使用variable参数来接收变量
+        check1 = tk.Checkbutton(check_box_frame, text="内容文本", variable=self.check_export_text, onvalue=1,
+                                offvalue=0)
+        check2 = tk.Checkbutton(check_box_frame, text="表格文本", variable=self.check_export_table, onvalue=1,
+                                offvalue=0)
+        check3 = tk.Checkbutton(check_box_frame, text="图片", variable=self.check_export_image, onvalue=1,
+                                offvalue=0)
+        check4 = tk.Checkbutton(check_box_frame, text="附件", variable=self.check_export_attachment, onvalue=1,
+                                offvalue=0)
+        check5 = tk.Checkbutton(check_box_frame, text="合并文本表格", variable=self.check_export_attachment, onvalue=1,
+                                offvalue=0)
+
+        # 选择第一个为默认选项
+
+        check1.select()
+        check2.select()
+        check3.select()
+        check4.select()
+        check5.select()
+
+        check1.grid(row=0, column=0)
+        check2.grid(row=0, column=1)
+        check3.grid(row=0, column=2)
+        check4.grid(row=0, column=3)
+        check5.grid(row=0, column=4)
+
+        check_box_frame.grid(row=4, column=0, columnspan=3)
+
+        export_setting_frame.pack()
+
+        # 导出按钮=====================================================================================================
+
+        # 导出按钮
+        tk.Button(main_right_frame,
+                  text="导出",
+                  fg="red",
+                  width=20, height=1,
+                  command=self.export) \
+            .pack()
+
+        tk.Label(main_right_frame, text='批量导出docx文本、图片和附件程序\n' + self.version, font=('宋体', 18, 'bold italic'),
+                 bg='#7CCD7C',
+                 # 设置标签内容区大小
+                 width=34, height=2,
+                 # 设置填充区距离、边框宽度和其样式（凹陷式）
+                 padx=10, pady=15, borderwidth=10, relief='sunken').pack(side=tk.BOTTOM)
+
+        main_right_frame.grid(row=0, column=1, sticky='n')
+
+    def choose_export_dir(self):
+        directory = filedialog.askdirectory()
+        if directory:
+            self.entry_export_dir_val.set(directory)
+
+    def export(self):
+        # 没有选择任何项目的情况下
+        if (check_var1.get() == 0 and check_var1.get() == 0 and check_var1.get() == 0):
+            s = '您还没选择任语言'
+        else:
+            s1 = "Python" if check_var1.get() == 1 else ""
+            s2 = "C语言" if check_var1.get() == 1 else ""
+            s3 = "Java" if check_var1.get() == 1 else ""
+            s = "您选择了%s %s %s" % (s1, s2, s3)
+
+    def remove_list_item(self):
+        if len(self.main_listbox.curselection()) == 0:
+            showwarning('错误', '没有选中任何条目')
+        else:
+            remove_index_list = []
+            for index in self.main_listbox.curselection():
+                remove_index_list.append(index)
+                self.main_listbox.select_clear(index)
+
+            # 需要从后面开始删除
+            remove_index_list.sort(reverse=True)
+
+            remove_file_list = []
+            for index in remove_index_list:
+                item_text = self.main_listbox.get(index)
+                if item_text:
+                    file_path = str(item_text).split('|-', 1)[1]
+                    remove_file_list.append(file_path)
+                    self.main_listbox.delete(index)
+
+            for file_path in remove_file_list:
+                self.file_list.remove(file_path)
+
+    def add_file_list(self, files: list):
         success = 0
-        index = len(self.file_list)
+        print(self.file_list)
 
         # 序号前面补 0
-        bit = len(str(index + len(file_list)))
+        bit = len(str(len(files)))
         if bit < 4:
             bit = 4
 
-        for file in file_list:
-            index += 1
+        for file in files:
+            # 已添加，不需要再添加
+            if file in self.file_list:
+                continue
+            self.file_index += 1
             logger.debug('添加文件：%s', file)
-            self.main_listbox.insert('end', str(index).rjust(bit, '0') + ' - ' + file)  # 从最后一个位置开始加入值
+            self.main_listbox.insert('end', str(self.file_index).rjust(bit, '0') + '|-' + file)  # 从最后一个位置开始加入值
             self.file_list.append(file)
             success += 1
         showinfo('添加结果', '添加成功{0}个'.format(success))
-
-    def exit_app(self):
-        if askokcancel('退出', '是否退出程序？'):
-            self.master.destroy()
-            quit()
 
     def choose_file(self):
         files_tuple = filedialog.askopenfilename(title='请选择docx文件', filetypes=[('Word', '.docx')],
@@ -129,7 +329,6 @@ class Application(tk.Frame):
             file_list = []
             for file in files_tuple:
                 if file_check(file):
-                    self.file_list.append(file)
                     file_list.append(file)  # 添加到列表中
             self.add_file_list(file_list)
 
@@ -153,6 +352,16 @@ class Application(tk.Frame):
 def run():
     root = tk.Tk()
     app = Application(master=root, version=v)
+
+    def closeWindow():
+        ans = askyesno(title='提示', message='是否关闭窗口？')
+        if ans:
+            root.destroy()
+        else:
+            return
+
+    root.protocol('WM_DELETE_WINDOW', closeWindow)
+
     root.title('导出docx')
     root.iconbitmap('images/icon.ico')
     app.mainloop()
